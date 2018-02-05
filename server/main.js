8a698d3883e1d9b8a8aa7fbb29247445d2728e49
Fabio Hildebrand <polaco@Fabios-MacBook-Pro.local>
2018-02-05 18:32:33 -0200

    ajustes, publicação no heroku

--- server/main.js
+++ server/main.js
@@ -9,6 +9,7 @@ Meteor.startup(() => {
 Accounts.validateNewUser(function (user) {
   if (user.services.google.email.search("@echos.cc") > 0 || user.services.google.email == "ndafpi4bsp@gmail.com") {
     return true;
+
   }
   throw new Meteor.Error(403, "Use o seu email @echos.cc");
 });
