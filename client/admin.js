69672350c6c5b3d9cc76a6f39e2dd67c6bb1d853
Fabio Hildebrand <polaco@Fabios-MacBook-Pro.local>
2018-02-16 11:50:44 -0200

    admin - consultando usuários, layout

--- client/admin.js
+++ client/admin.js
@@ -1,4 +1,18 @@
+import datatables from 'datatables.net';
+import datatables_bs from 'datatables.net-bs';
+import 'datatables.net-bs/css/dataTables.bootstrap.css';
+
+Template.admin.events({
+  'click .user-address' (event, instance) {
+    event.preventDefault();
+    window.open("https://rinkeby.etherscan.io/address/" + event.target.innerHTML);
+  },
+});
+
 Template.admin.onRendered(function () {
+  datatables(window, $);
+  datatables_bs(window, $);
+
   // Web3 stuff
 
   // Init web3
@@ -15,204 +29,53 @@ Template.admin.onRendered(function () {
     console.log("infura")
   }
 
-  var contractAddress = "0xc5452d2f42c4ad570f3af3b492e48d9cf10cf998";
-  var contractAbi = [
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "unpause",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "tokenContract",
-          "type": "address"
-			}
-		],
-      "name": "withdrawTokens",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "paused",
-      "outputs": [
-        {
-          "name": "",
-          "type": "bool"
-			}
-		],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-	},
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "withdrawBalance",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_name",
-          "type": "string"
-			},
-        {
-          "name": "_email",
-          "type": "string"
-			},
-        {
-          "name": "_course",
-          "type": "string"
-			},
-        {
-          "name": "_hours",
-          "type": "uint256"
-			},
-        {
-          "name": "_dates",
-          "type": "string"
-			}
-		],
-      "name": "printCertificate",
-      "outputs": [
-        {
-          "name": "_certificateAddress",
-          "type": "address"
-			}
-		],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "pause",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_user",
-          "type": "address"
-			},
-        {
-          "name": "_transaction",
-          "type": "uint256"
-			}
-		],
-      "name": "grantAccess",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_certificateContract",
-          "type": "address"
-			}
-		],
-      "name": "invalidateCertificate",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_user",
-          "type": "address"
-			},
-        {
-          "name": "_transaction",
-          "type": "uint256"
-			}
-		],
-      "name": "revokeAccess",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-	},
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "name": "_user",
-          "type": "address"
-			},
-        {
-          "name": "_transaction",
-          "type": "uint256"
-			}
-		],
-      "name": "hasAccess",
-      "outputs": [
-        {
-          "name": "",
-          "type": "bool"
-			}
-		],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-	},
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "name": "contractAddress",
-          "type": "address"
-			},
-        {
-          "indexed": false,
-          "name": "_name",
-          "type": "string"
-			},
-        {
-          "indexed": false,
-          "name": "email",
-          "type": "string"
-			},
-        {
-          "indexed": false,
-          "name": "_course",
-          "type": "string"
-			},
-        {
-          "indexed": false,
-          "name": "_dates",
-          "type": "string"
-			}
-		],
-      "name": "logPrintedCertificate",
-      "type": "event"
-	}
-  ]
-
   echosContract = web3js.eth.contract(contractAbi).at(contractAddress);
+
+  // append row to the HTML table
+  function appendRow(data) {
+    var accessRight;
+    if (data[1] == 1)
+      accessRight = "Acesso";
+    else if (data[1] == 2)
+      accessRight = "Financeiro";
+    else if (data[1] == 3)
+      accessRight = "Certificados";
+    $("#access-rights").DataTable().row.add([
+      "<a class='user-address' style='cursor: pointer;' href'#' >" + data[0] + "</a>",
+      accessRight,
+      "<span id='" + data[0] + "'><i class='fa fa-spinner fa-pulse'></i><span>"
+    ]).draw();
+  }
+
+  // Get current users
+  var users = echosContract.accessGranted({}, {
+    fromBlock: 1600000,
+    toBlock: 'latest'
+  });
+
+  users.watch(function (error, result) {
+
+    if (!error) {
+      var data = [
+        result.args.user,
+        result.args.access.toString(10),
+        result.blockNumber,
+        result.transactionHash
+      ]
+
+      appendRow(data);
+
+    } else {
+      console.log(error);
+    }
+  });
+
+
+  // data table
+  $('#access-rights').dataTable({
+    "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json"
+    }
+  });
+
 })
