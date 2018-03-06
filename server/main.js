28581b6b2c08ef7047273e2a388599c1a5e61cc7
Fabio Hildebrand <polaco@Fabios-MacBook-Pro.local>
2018-03-06 15:25:22 -0300

    ajustando para o email da echos

--- server/main.js
+++ server/main.js
@@ -14,13 +14,13 @@ Accounts.validateNewUser(function (user) {
   throw new Meteor.Error(403, "Use o seu email @echos.cc");
 });
 
-process.env.MAIL_URL = "smtp://ndafpi4bsp@gmail.com:shpongle@smtp.gmail.com:587/";
+process.env.MAIL_URL = "smtp://oi@echos.cc:starwars1.@smtp.gmail.com:587/";
 
 Meteor.methods({
   'sendEmail' (to, title, msg) {
     Email.send({
-      to: "fabiohildebrand@gmail.com",
-      from: 'ndafpi4bsp@gmail.com',
+      to: to,
+      from: 'oi@echos.cc',
       subject: title,
       html: msg
     });
