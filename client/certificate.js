3785acf55bddaa4c230458bd2913c77eccbdc4fc
fabiohild <fabiohildebrand@gmail.com>
2018-05-11 15:43:43 -0300

    certificate viewer implementedte

---
+++ client/certificate.js
@@ -0,0 +1,60 @@
+Template.certificate.helpers({
+  certificateAddress() {
+    return Router.current().params.certificateAddress;
+  },
+  invalid() {
+    return Session.get("invalid");
+  },
+});
+
+Template.certificate.events({
+  'click #printPDF' (event, instance) {
+    html2canvas(window.document.getElementById("certificate"), {
+      scale: 2
+    }).then(function (canvas) {
+      var a = document.createElement('a');
+      // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+      a.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      a.download = 'Bizancio - Certificado.png';
+      a.click();
+    });
+  },
+})
+
+Template.certificate.onCreated(function () {
+  // Web3 stuff
+
+  // Init web3
+  var Web3 = require('web3');
+
+  if (typeof web3 !== 'undefined') {
+    web3js = new Web3(web3.currentProvider);
+    console.log("injected web3");
+  } else {
+    //set the provider to Rinkeby/Infura, will only work for view functions
+    web3js = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/kak6M2Qgf7oHycGaCI2E"));
+    console.log("infura")
+  }
+
+  var bizancioContract = web3js.eth.contract(contractAbi).at(contractAddress);
+
+
+  // get certificate data
+  bizancioContract.certificates.call(Router.current().params.certificateAddress, function (error, result) {
+    if (!error) {
+      if (result[5] == false) {
+        Session.set("invalid", true)
+        return;
+      }
+
+      if (result[2] == "Impacta - Workshop Blockchain")
+        document.getElementById("diploma").src = "/CertificadoDTS.png";
+
+      document.getElementById("name").textContent = result[0];
+      document.getElementById("course").textContent = result[2] + " é um curso da Impacta em parceria com a Bizanc.io com duração de " + result[4].c[0] + " horas.";
+      document.getElementById("dates").textContent = "Realizado nas datas " + result[3] + ".";
+
+    } else
+      console.error(error);
+  });
+});
--- server/main.js
+++ server/main.js
@@ -6,23 +6,23 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
-Accounts.validateNewUser(function (user) {
-  if (user.services.google.email.search("@echos.cc") > 0 || user.services.google.email == "ndafpi4bsp@gmail.com") {
-    return true;
+//Accounts.validateNewUser(function (user) {
+//  if (user.services.google.email.search("@echos.cc") > 0 || user.services.google.email == "ndafpi4bsp@gmail.com") {
+//    return true;
+//
+//  }
+//  throw new Meteor.Error(403, "Use o seu email @echos.cc");
+//});
 
-  }
-  throw new Meteor.Error(403, "Use o seu email @echos.cc");
-});
-
-process.env.MAIL_URL = "smtp://oi@echos.cc:starwars1.@smtp.gmail.com:587/";
-
+//process.env.MAIL_URL = "smtp://oi@echos.cc:password@smtp.gmail.com:587/";
+//
 Meteor.methods({
   'sendEmail' (to, title, msg) {
-    Email.send({
-      to: to,
-      from: 'oi@echos.cc',
-      subject: title,
-      html: msg
-    });
+    //    Email.send({
+    //      to: to,
+    //      from: 'oi@echos.cc',
+    //      subject: title,
+    //      html: msg
+    //    });
   }
 });
