8eaac0a9340eba975d43f5695317d67f8b1a6075
fabiohild <fabiohildebrand@gmail.com>
2019-01-14 16:49:24 -0200

    home layout

--- client/certificate.js
+++ client/certificate.js
@@ -28,7 +28,7 @@ Template.certificate.events({
             var a = document.createElement('a');
             // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
             a.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-            a.download = 'Bizancio - Certificado.png';
+            a.download = 'GBC - Certificado.png';
             a.click();
         });
     },
--- server/main.js
+++ server/main.js
@@ -6,21 +6,9 @@ Meteor.startup(() => {
     // code to run on server at startup
 });
 
-//Accounts.validateNewUser(function (user) {
-//  if (user.services.google.email.search("@echos.cc") > 0 || user.services.google.email == "ndafpi4bsp@gmail.com") {
-//    return true;
-//
-//  }
-//  throw new Meteor.Error(403, "Use o seu email @echos.cc");
-//});
-
-//process.env.MAIL_URL = "smtp://oi@echos.cc:password@smtp.gmail.com:587/";
-//
 Meteor.methods({
     'uploadPic' (file, institution, address) {
-        // async issues in method call, research
-        //accessControlContract.hasRole.call(address, institution, function (error, result) {
-        //if (result) {
+
         var imagesURL = {
             "image": "/cfs/files/images/" + file
         };
@@ -37,9 +25,7 @@ Meteor.methods({
                 address: institution,
                 image: imagesURL
             });
-        //}
 
-        //});
     }
 });
 
